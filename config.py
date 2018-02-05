d92dd7c81f98cd1cb2b71657834b6f457cb22b8a
Horst Schneider <horst.schneider@posteo.de>
2018-02-06 00:09:18 +0100

    Add all outcome rows

---
+++ config.py
@@ -0,0 +1,20 @@
+config = {
+    "f_bruttolohn": 3000,
+    "f_abrechnungszeitraum": "monat",
+    "f_geld_werter_vorteil": None, # [None|<number>]
+    "f_abrechnungsjahr": 2017,
+    "f_steuerfreibetrag": None, # [None|<number>]
+    "f_steuerklasse": 3,
+    "f_kirche": "ja",
+    "f_bundesland": "baden-wuerttemberg",
+    "f_alter": 25,
+    "f_kinder": "nein",
+    "f_kinderfreibetrag": 0,
+    "f_krankenversicherung": "pflichtversichert",
+    "f_private_kv": None, # [None|<number>]
+    "f_arbeitgeberzuschuss_pkv": "ja",
+    "f_KVZ": 1.1,
+    "f_rentenversicherung": "pflichtversichert",
+    "f_arbeitslosenversicherung": "pflichtversichert",
+    "ok": 1
+}
--- gnc.py
+++ gnc.py
@@ -1,100 +1,130 @@
 #! /usr/bin/env python3
-import requests
-from bs4 import BeautifulSoup
+import argparse
 import locale
+import requests
+from config import config
+from bs4 import BeautifulSoup
+
 locale.setlocale(locale.LC_ALL, 'de_DE.UTF8')
+
+
+class Euros(object):
+    """Represents Euros, formats them nicely"""
+
+    def __init__(self, amount):
+        self.__amount = amount
+
+    def amount(self):
+        """Numeric Euros amount"""
+        return self.__amount
+
+    def __str__(self):
+        return locale.currency(self.__amount)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __format__(self, format_spec):
+        return format(str(self), format_spec)
 
 
 class GrossToNet(object):
 
-    def __init__(self):
+    def __init__(self, config):
         self.__url = "https://www.brutto-netto-rechner.info/"
+        self.__config = config
 
-    def calculate_for(self, params):
+    def calculate_for(self, monthly_gross):
+        """Calculates monthly net from monthly gross"""
+        params = {**self.__config, **{"f_bruttolohn": monthly_gross}}
         response = requests.post(self.__url, params)
         html = BeautifulSoup(response.text, 'html.parser')
-        return Result(html.find("table", attrs={"class": "rechner"}))
+        return Outcome(html.find("table", attrs={"class": "rechner"}))
 
 
-class Result(object):
+class Outcome(object):
 
     def __init__(self, html):
         self.__html = html
 
     def gross_per_month(self):
+        """Brutto (pro Monat)"""
         return self.__get_amount_from_row(2)
 
     def non_cash_benefit(self):
+        """Geldwerter Vorteil"""
         return self.__get_amount_from_row(3)
 
     def church_tax(self):
+        """Kirchensteuer"""
         return self.__get_amount_from_row(7)
 
     def solidarity(self):
+        """Solidaritätszuschlag"""
         return self.__get_amount_from_row(6)
 
     def wage_tax(self):
+        """Lohnsteuer"""
         return self.__get_amount_from_row(8)
 
+    def taxes(self):
+        """Gesamtsteuern (Lohnsteuer + Kirchensteuer + Solidaritätszuschlag)"""
+        return self.__get_amount_from_row(10)
+
     def pension_insurance(self):
+        """Rentenversicherung"""
         return self.__get_amount_from_row(13)
 
     def unemployment_insurance(self):
+        """Arbeitslosenversicherung"""
         return self.__get_amount_from_row(15)
-    
+
     def health_insurance(self):
+        """Krankenversicherung"""
         return self.__get_amount_from_row(16)
 
     def nursing_care_insurance(self):
+        """Pflegeversicherung"""
         return self.__get_amount_from_row(17)
-    
-    def taxes(self):
-        return self.wage_tax() + self.church_tax() + self.solidarity()
 
     def insurances(self):
-        return self.pension_insurance() + self.unemployment_insurance() + self.health_insurance() + self.nursing_care_insurance()
+        """Sozialabgaben"""
+        return self.__get_amount_from_row(19)
 
     def net_per_month(self):
+        """Netto (pro Monat)"""
         return self.__get_amount_from_row(21)
 
     def __get_amount_from_row(self, row_num):
-        raw_value = str(self.__html.find_all("tr")[row_num].find(
-            "td", attrs={"class": "right_column"}).text)
-        return locale.atof(raw_value.strip().strip("€"))
+        row = self.__html.find_all("tr")[row_num]
+        amount = row.find("td", attrs={"class": "right_column"}).text
+        return Euros(locale.atof(str(amount).strip().strip("€")))
 
-
-params = {
-    "f_bruttolohn": 3000,
-    "f_abrechnungszeitraum": "monat",
-    "f_geld_werter_vorteil": None,
-    "f_abrechnungsjahr": "2018",
-    "f_steuerfreibetrag": None,
-    "f_steuerklasse": 3,
-    "f_kirche": "ja",
-    "f_bundesland": "baden-wuerttemberg",
-    "f_alter": 25,
-    "f_kinder": "nein",
-    "f_kinderfreibetrag": 0,
-    "f_krankenversicherung": "pflichtversichert",
-    "f_private_kv": None,
-    "f_arbeitgeberzuschuss_pkv": "ja",
-    "f_KVZ": 1.1,
-    "f_rentenversicherung": "pflichtversichert",
-    "f_arbeitslosenversicherung": "pflichtversichert",
-    "ok": 1
-}
 
 if __name__ == "__main__":
-    result = GrossToNet().calculate_for(params)
-    print("Brutto pro Monat: %s" % locale.currency(result.gross_per_month()))
-    print("Geldwerter Vorteil: %s" % locale.currency(result.solidarity()))
-    print("Solidaritätszuschlag: %s" % locale.currency(result.solidarity()))
-    print("Kirchensteuer: %s" % locale.currency(result.church_tax()))
-    print("Lohnsteuer: %s" % locale.currency(result.wage_tax()))
-    print("Steuern: %s" % locale.currency(result.taxes()))
-    print("Rentenversicherung: %s" % locale.currency(result.pension_insurance()))
-    print("Arbeitslosenversicherung: %s" % locale.currency(result.unemployment_insurance()))
-    print("Krankenversicherung: %s" % locale.currency(result.health_insurance()))
-    print("Pflegeversicherung: %s" % locale.currency(result.nursing_care_insurance()))
-    print("Sozialabgaben: %s" % locale.currency(result.insurances()))
-    print("Netto pro Monat: %s" % locale.currency(result.net_per_month()))
+    parser = argparse.ArgumentParser(prog="gnc",
+                                     description='Convert monthly gross to net.',
+                                     epilog="Change calculation params in config.py accordingly.")
+    parser.add_argument('gross',
+                        metavar="<gross>",
+                        type=int,
+                        help='monthly gross (in euros)')
+
+    monthly_gross = parser.parse_args().gross
+
+    result = GrossToNet(config).calculate_for(monthly_gross)
+
+    print(" {: >12}\t Brutto pro Monat".format(result.gross_per_month()))
+    print("+{: >12}\t Geldwerter Vorteil\n".format(result.non_cash_benefit()))
+    print(" {: >12}\t Solidaritätszuschlag".format(result.solidarity()))
+    print(" {: >12}\t Kirchensteuer".format(result.church_tax()))
+    print(" {: >12}\t Lohnsteuer".format(result.wage_tax()))
+    print("-{: >12}\t Steuern\n".format(result.taxes()))
+    print(" {: >12}\t Rentenversicherung".format(result.pension_insurance()))
+    print(" {: >12}\t Arbeitslosenversicherung".format(
+        result.unemployment_insurance()))
+    print(" {: >12}\t Krankenversicherung".format(result.health_insurance()))
+    print(" {: >12}\t Pflegeversicherung".format(
+        result.nursing_care_insurance()))
+    print("-{: >12}\t Sozialabgaben\n".format(result.insurances()))
+    print("={: >12}\t Netto pro Monat".format(result.net_per_month()))
